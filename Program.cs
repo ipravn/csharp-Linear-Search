71cb938cb801f14236c6a9c8f455cf34008e7bbc
Praveen Kumar <46933956+ipravn@users.noreply.github.com>
2022-07-29 16:06:41 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,50 @@
+// Linear Search Algorithm Using C#
+
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, };
+        int target = 10;
+        int ans = Liniarsearch(arr,target);
+        Console.WriteLine(ans);
+        Console.ReadKey();
+       
+    }
+
+    static int Liniarsearch(int[] arr, int target)
+    {
+        if (arr.Length == 0)
+        {
+            return -1;
+        }
+        for (int i = 0; i < arr.Length; i++)
+        { 
+            int element = arr[i];
+            if (element == target)
+            { 
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
